7295b9e2d2817274e03a635a3a64841e956be812
micah@work <michaeleschbacher@gmail.com>
2012-10-29 13:22:00 -0500

    Initial commit - skeleton

---
+++ Histogram.php
@@ -0,0 +1,32 @@
+<?php
+class Histogram
+{
+	protected $values = array();
+	public function __construct($val1 = null)
+	{
+
+	}
+	public function addValue($value)
+	{
+
+	}
+	
+	public function buildFromArray(array $ary = array(), $key = null)
+	{
+
+	}
+
+	public function buildFromObjects(array $ary = array(), $property)
+	{
+
+	}
+
+	public function getValues()
+	{
+
+	}
+	public function order($inverse = 0)
+	{
+
+	}
+}
