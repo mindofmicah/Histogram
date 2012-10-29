0953aa61557f9b6085e084d7513ea432758a99e6
micah@work <michaeleschbacher@gmail.com>
2012-10-29 13:45:23 -0500

    Added tests for the getter, constructor, and the method to add values to the object

--- TestHistogram.php
+++ TestHistogram.php
@@ -5,12 +5,27 @@ class TestHistogram extends PHPUnit_Framework_TestCase
     protected $values = array();
     public function testConstructor()
     {
-
+		$this->assertInstanceOf('Histogram', new Histogram());
     }
-    public function testAddValue()
+	public function testConstructorWithValue()
+	{
+		$histogram = new Histogram(3,5);
+		$this->assertEquals(array(3=>1, 5=>1), $histogram->getValues());
+	}
+    
+	public function testAddValueNewValue()
     {
-
+		$histogram = new Histogram();
+		$histogram->addValue(3);
+		$this->assertEquals(array(3=>1), $histogram->getValues());
     }
+
+	public function testAddValueExistingEntry()
+	{
+		$histogram = new Histogram(3);
+		$histogram->addValue(3);
+		$this->assertEquals(array(3=>2), $histogram->getValues());
+	}
 
     public function testBuildFromArray()
     {
@@ -24,7 +39,11 @@ class TestHistogram extends PHPUnit_Framework_TestCase
 
     public function testGetValues()
     {
-
+		$histogram = new Mock_Histogram();
+		$this->assertEquals(array(), $histogram->getValues());
+	
+		$histogram->values = array(1=>3);
+		$this->assertEquals(array(1=>3), $histogram->getValues());
     }
     public function testOrder()
     {
