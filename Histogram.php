e4f3dda0db1c6ad30286a28d453cc8ff87be586a
micah@work <michaeleschbacher@gmail.com>
2012-10-29 13:46:01 -0500

    Made the tests pass for the constructor, getter, and the mutilator

--- Histogram.php
+++ Histogram.php
@@ -4,11 +4,18 @@ class Histogram
 	protected $values = array();
 	public function __construct($val1 = null)
 	{
-
+		$args = func_get_args();
+		for ($i = 0, $length = count($args); $i < $length; $i++) {
+			$this->addValue($args[$i]);
+		}
 	}
 	public function addValue($value)
 	{
-
+		if (array_key_exists($value, $this->values)) {
+			$this->values[$value]++;
+		} else {
+			$this->values[$value] = 1;
+		}
 	}
 	
 	public function buildFromArray(array $ary = array(), $key = null)
@@ -23,7 +30,7 @@ class Histogram
 
 	public function getValues()
 	{
-
+		return $this->values;
 	}
 	public function order($inverse = 0)
 	{
